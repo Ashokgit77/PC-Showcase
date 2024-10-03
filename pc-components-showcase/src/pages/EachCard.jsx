5b1dcad7dd788fd75cf47821cbe898f902743fd0
Ashokgit77 <ashok.iac02@gmail.com>
2024-10-03 13:13:37 +0530

    animation added

--- pc-components-showcase/src/pages/EachCard.jsx
+++ pc-components-showcase/src/pages/EachCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
 
 const EachCard = () => {
   const location = useLocation();
@@ -7,16 +8,29 @@ const EachCard = () => {
 
   const wikiUrl = `https://en.wikipedia.org/wiki/${sTitle.replace(/ /g, "_")}`;
 
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleIframeLoad = () => {
+    setIsLoading(false);
+  };
+
   return (
     <div className="p-6">
       <h1 className="text-3xl font-bold mb-6 text-center bg-blue-900 p-2 text-white rounded-lg">
         {title}
       </h1>
 
+      {isLoading && (
+        <div className="w-full h-screen bg-gray-200 rounded-lg animate-pulse flex items-center justify-center"></div>
+      )}
+
       <iframe
         src={wikiUrl}
         title={sTitle}
-        className="w-full h-screen border-0"
+        className={`w-full h-screen border-0 rounded-sm ${
+          isLoading ? "hidden" : "block"
+        }`}
+        onLoad={handleIframeLoad}
       ></iframe>
     </div>
   );
